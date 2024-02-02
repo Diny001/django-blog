a7e99d8d42962592752e1604be81f74a2aaeb121
Dinesh <dinydinesh001@gmail.com>
2024-02-02 12:57:19 +0530

    blogs and search features

--- blogs/context_processors.py
+++ blogs/context_processors.py
@@ -1,6 +1,11 @@
+from assingments.models import SocialLink
 from .models import Category
 
 
 def get_categories(request):
     categories= Category.objects.all()
     return dict(categories=categories)
+
+def get_socaillinks(request):
+    sociallinks=SocialLink.objects.all()
+    return dict(sociallinks=sociallinks)

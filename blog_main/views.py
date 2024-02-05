cdb586e104ed779c160f13ec4ff23dd4da8e241b
Dinesh <dinydinesh001@gmail.com>
2024-02-05 13:34:33 +0530

    registation and login function

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import RegiestrationForm
 
 from blogs.models import Blog, Category
 from assingments.models import About
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 
 def home(request):
@@ -20,3 +23,42 @@ def home(request):
         'about':about,
     }
     return render(request, 'home.html',context)
+
+def register(request):
+    if request.method== 'POST':
+        form= RegiestrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+        else:
+            print(form.errors)
+    else:
+        form=RegiestrationForm()
+ 
+    context={
+        'form':form
+    }
+    return render(request,'register.html',context)
+
+def login(request):
+    if request.method== 'POST':
+        form = AuthenticationForm(request,request.POST)
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password']
+
+            user=auth.authenticate( username=username, password=password)
+
+            if user is not None:
+                auth.login(request,user)
+            return redirect('home')
+
+    form= AuthenticationForm()
+    context={
+        'form':form,
+    }
+    return render(request, 'login.html',context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
